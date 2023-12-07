0a72d7cfe0b78da85b845bc8af5187f392101380
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-12-07 13:27:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+  const myElement = document.createElement("p");
+  myElement.innerHTML = "DOM load success";
+  this.body.appendChild(myElement);
+});
